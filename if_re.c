2a6a7e2b1566f58855639ba36de4e0cc6b2b878d
Alex Dupre <ale@FreeBSD.org>
2020-07-20 15:28:16 +0200

    Add support for 13-CURRENT.

--- if_re.c
+++ if_re.c
@@ -8618,6 +8618,22 @@ struct re_softc		*sc;
         return;
 }
 
+#if OS_VER >= VERSION(13,0)
+static u_int
+re_hash_maddr(void *arg, struct sockaddr_dl *sdl, u_int cnt)
+{
+	uint32_t h, *hashes = arg;
+
+	h = ether_crc32_be(LLADDR(sdl), ETHER_ADDR_LEN) >> 26;
+	if (h < 32)
+		hashes[0] |= (1 << h);
+	else
+		hashes[1] |= (1 << (h - 32));
+
+	return (1);
+}
+#endif
+
 /*
  * Program the 64-bit multicast hash filter.
  */
@@ -8627,7 +8643,9 @@ struct re_softc		*sc;
         struct ifnet		*ifp;
         int			h = 0;
         u_int32_t		hashes[2] = { 0, 0 };
+#if OS_VER < VERSION(13,0)
         struct ifmultiaddr	*ifma;
+#endif
         u_int32_t		rxfilt;
         int			mcnt = 0;
 
@@ -8644,6 +8662,9 @@ struct re_softc		*sc;
         }
 
         /* now program new ones */
+#if OS_VER >= VERSION(13,0)
+	if_foreach_llmaddr(ifp, re_hash_maddr, hashes);
+#else
 #if OS_VER >= VERSION(12,0)
 	if_maddr_rlock(ifp);
 #elif OS_VER > VERSION(6,0)
@@ -8672,6 +8693,7 @@ struct re_softc		*sc;
 	if_maddr_runlock(ifp);
 #elif OS_VER > VERSION(6,0)
         IF_ADDR_UNLOCK(ifp);
+#endif
 #endif
 
         if (mcnt) {
